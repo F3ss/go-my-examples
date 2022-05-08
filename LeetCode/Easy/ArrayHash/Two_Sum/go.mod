0d980cb0e1f8810c92c0ac9ae2c9d7798b033adb
F3ss <viktorfess@gmail.com>
2022-05-08 08:48:24 +0300

    Solved by map

---
+++ LeetCode/Easy/ArrayHash/Two_Sum/go.mod
@@ -0,0 +1,3 @@
+module mainmodul
+
+go 1.18
--- LeetCode/Easy/ArrayHash/Two_Sum/main.go
+++ LeetCode/Easy/ArrayHash/Two_Sum/main.go
@@ -6,21 +6,26 @@ import (
 
 func main() {
 	fmt.Println(Two_Sum([]int{1, 7, 11, 2}, 9))
+	fmt.Println(Two_Sum([]int{3, 3}, 6))
 }
 
 func Two_Sum(nums []int, target int) []int {
+	var num int
 	mapa := make(map[int]int, 100)
 
-	for index, i := range nums {
-		num := target - i
+	for i := 0; i < len(nums); i++ {
+		num = target - nums[i]
 
-		if _, has := mapa[num]; has == false {
-			mapa[num] = index
+		if _, has := mapa[num]; !has {
+			mapa[num] = i
+		}
+
+		if _, has := mapa[nums[i]]; !has || has && mapa[nums[i]] == i {
 			continue
 		}
-		return []int{index, mapa[num]}
+		return []int{mapa[target-num], i}
 	}
-	fmt.Println(mapa)
+
 	return nil
 }
 
