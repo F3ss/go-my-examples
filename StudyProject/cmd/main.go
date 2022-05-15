7b6939af6ac411e609d9ae65dfd369e13de5003b
F3ss <viktorfess@gmail.com>
2022-05-15 09:00:31 +0300

    my logger

--- StudyProject/cmd/main.go
+++ StudyProject/cmd/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home page")
+	parseTemplate, err := template.ParseFiles("./home_page.html")
+	if err != nil {
+		log.Fatalf("Error parsing template: %s\n", err.Error())
+	}
+
+	parseTemplate.Execute(w, nil)
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About page")
-}
+	parseTemplate, err := template.ParseFiles("./about_page.html")
+	if err != nil {
+		log.Fatalf("Error parsing template: %s\n", err.Error())
+	}
 
-func AddValues(x, y int) int {
-	return x + y
+	parseTemplate.Execute(w, nil)
 }
 
 func main() {
